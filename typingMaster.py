2dd0ba9ac8dfab69b1faa13096cad75c00449591
juanRuiz <juanca.ruiz2010@gmail.com>
2026-02-18 22:49:15 -0400

    added confirmation for the word and +1 to counters

--- typingMaster.py
+++ typingMaster.py
@@ -7,5 +7,14 @@ try:
         print(f"The word is {word}.")
         userInput = input("Type: ")
         # I don't do .lower() because you gotta write it perfectly
+        if userInput == word:
+            numCorrect += 1
+            print("You typed it correctly!")
+        else:
+            numIncorrect += 1
+            print("You typed it incorrectly...")
+    print(f"\nTotal Correct: {numCorrect}")
+    print(f"Total Incorrect: {numIncorrect}")
+ 
 except Exception as e:
     print("An unexpected error occurred:", e)
