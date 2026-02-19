82442de1633d1eeb147f1428e9626a84ca709553
juanRuiz <juanca.ruiz2010@gmail.com>
2026-02-18 22:48:40 -0400

    added the loop for each word

--- typingMaster.py
+++ typingMaster.py
@@ -2,6 +2,10 @@ words = ["Accomplishments", "Benzodiazepines", "Cyclohexylamine", "Discombobulat
 try:
     numCorrect = 0
     numIncorrect = 0
-    
+    for word in words:
+        print("Type each word correctly! (Case sensitive)")
+        print(f"The word is {word}.")
+        userInput = input("Type: ")
+        # I don't do .lower() because you gotta write it perfectly
 except Exception as e:
     print("An unexpected error occurred:", e)
