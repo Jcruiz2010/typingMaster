b177de727f3645fafde30eb7f71c048c93b4654b
juanRuiz <juanca.ruiz2010@gmail.com>
2026-02-18 22:47:26 -0400

    added the words, error handling, and counters

---
+++ typingMaster.py
@@ -0,0 +1,7 @@
+words = ["Accomplishments", "Benzodiazepines", "Cyclohexylamine", "Discombobulated", "Disenfranchised"]
+try:
+    numCorrect = 0
+    numIncorrect = 0
+    
+except Exception as e:
+    print("An unexpected error occurred:", e)
